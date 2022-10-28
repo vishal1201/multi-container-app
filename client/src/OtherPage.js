d06163667c5aea659fbc44e0efb40621e37cd719
Vishal Zambare <vishalzambare5@gmail.com>
2022-10-28 10:35:09 +0000

    Change back to multi-container-dev version for K8S

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default OtherPage => function() {
+export default () => {
   return (
     <div>
       Im some other page!
