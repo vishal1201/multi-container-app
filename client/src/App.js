ce65e55fefff141cd6c4a6b999043b3f60c0a03e
Vishal Zambare <vishalzambare5@gmail.com>
2022-10-28 09:26:35 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <h1>
-            Fib Calculator Version 2
+            Fib Calculator Version 3
           </h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
